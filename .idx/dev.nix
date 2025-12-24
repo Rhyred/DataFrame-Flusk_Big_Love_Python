1f3abb7670114c9371362d350ada14b64cab4b27
robigold9 <robigold9@gmail.com>
2025-12-24 01:28:01 +0000

    Changes before Firebase Studio auto-run

---
+++ .idx/dev.nix
@@ -0,0 +1,54 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python312
+    pkgs.python312Packages.flask
+    pkgs.python312Packages.pandas
+    pkgs.python312Packages.matplotlib
+    pkgs.python312Packages.numpy
+    pkgs.python312Packages.werkzeug
+    pkgs.python312Packages.jinja2
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["python3" "app.py"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
